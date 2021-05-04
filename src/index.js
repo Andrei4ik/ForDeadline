109e511dfd30afb5b3de682287d0a2749a043236
andrew <kerovap@mail.ru>
2021-05-04 14:59:29 +0700

    проблема с подключением через класс

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import Phaser from 'phaser';
 
 
-//import Game from './scenes/GameScenes'
+import GameScene from './scenes/GameScenes'
 //import Shop from './scenes/ShopScenes'
 //import Main from './scenes/MainScenes'
-
-let gameScene = new Phaser.Scene('Game');
-
 
 
 const config = {
@@ -15,49 +12,12 @@ const config = {
     width: window.innerWidth,
     height: window.innerHeight,
     backgroundColor: "#806546",  
-    rows: 10,
+    rows: 15,
     cols: 15, 
-    scene: gameScene 
+    scene: GameScene 
 };
 
 const game = new Phaser.Game(config);
 
-gameScene.preload = function(){
-    
-    this.load.image('grass','assets/dc-dngn/floor/dirt_full.png');
-    this.load.image('rock','assets/dc-dngn/floor/pebble_brown1.png');
-    this.load.image('water','assets/dc-dngn/water/dngn_deep_water.png');
-    this.load.image('dirt','assets/dc-dngn/floor/dirt0.png');
-    this.load.image('forest','assets/dc-dngn/floor/dirt_forest.png');
-    this.load.image('back','assets/back.png');
-    console.log("preload");
-};
 
 
-
-gameScene.create = function()
-{
-    this.add.sprite(275, 0, 'back').setOrigin(0,0);;
-
-    let positions = this.getFieldPositions();
-
-    for(let position of positions) {
-        this.add.sprite(position.x, position.y, 'grass').setOrigin(0,0);
-    }
-};
-
-scene.getFieldPositions = function() {
-    let positions = [];
-    let tileSize = 32;
-
-    for (let row = 0;  row < config.rows; row++) {
-        for (let col = 0; col < config.cols; col++){
-            positions.push({
-                x: col * tileSize+900,
-                y: row * tileSize+499,
-            });
-        }
-    }
-
-    return positions;
-}
---
+++ src/scenes/GameScene.js
@@ -0,0 +1,48 @@
+
+export class GameScene extends Phaser.Scene {
+    constructor() {
+        super ("Game");
+    }
+    preload() {
+        this.load.image('grass','assets/dc-dngn/floor/dirt_full.png');
+        this.load.image('rock','assets/dc-dngn/floor/pebble_brown1.png');
+        this.load.image('water','assets/dc-dngn/water/dngn_deep_water.png');
+        this.load.image('dirt','assets/dc-dngn/floor/dirt0.png');
+        this.load.image('forest','assets/dc-dngn/floor/dirt_forest.png');
+        this.load.image('back','assets/back.png');
+        console.log("preload");
+    }
+
+    create() {
+        this.add.sprite(275, 0, 'back').setOrigin(0,0);;
+
+        let positions = this.getFieldPositions();
+    
+        for(let position of positions) {
+            if(Math.random() > 0.4)
+                this.add.sprite(position.x, position.y, 'grass').setOrigin(0,0);
+            else
+                this.add.sprite(position.x, position.y, 'forest').setOrigin(0,0);
+        }
+    }
+
+    getFieldPositions() {
+        let positions = [];
+        let tileSize = 32;
+        let offsetx = 650;
+        let offsety = 250;
+    
+        for (let row = 0;  row < config.rows; row++) {
+            for (let col = 0; col < config.cols; col++){
+                positions.push({
+                    x: col * tileSize+offsetx,
+                    y: row * tileSize+offsety,
+                });
+            }
+        }
+    
+        return positions;
+    }
+}
+
+
